a7a96dd26cbc6b94f046c7db593b43d3a38e2ce7
Doyoung Kim <kimdoyoung0319@gmail.com>
2025-04-03 16:54:47 +0000

    Added fast version of the decomposition algorithm.

--- project1/src/lu-omp.cpp
+++ project1/src/lu-omp.cpp
@@ -2,120 +2,196 @@
 
 #include <algorithm>
 #include <chrono>
+#include <cmath>
 #include <cstdlib>
+#include <iomanip>
 #include <iostream>
 #include <random>
 
-double *target, *lower, *upper;
-size_t *permutation;
+double *a, *l, *u;
+size_t *p;
 int nworkers;
 
 void initialize(size_t size);
 void generate(size_t size);
 void run(size_t size);
+void verify(size_t size);
+void clean();
+void print(double *a, size_t size);
 
 void usage(const char *name) {
-  std::cout << "usage: " << name << " matrix-size nworkers" << std::endl;
-  exit(-1);
+    std::cout << "usage: " << name << " matrix-size nworkers" << std::endl;
+    exit(-1);
 }
 
 int main(int argc, char **argv) {
-  const char *name = argv[0];
+    using std::chrono::duration_cast;
+    using std::chrono::milliseconds;
+    using std::chrono::steady_clock;
 
-  if (argc < 3)
-    usage(name);
+    const char *name = argv[0];
 
-  int size = atoi(argv[1]);
+    if (argc < 3)
+        usage(name);
 
-  nworkers = atoi(argv[2]);
+    int size = atoi(argv[1]);
 
-  std::cout << name << ": " << size << " " << nworkers << std::endl;
+    nworkers = atoi(argv[2]);
 
-  omp_set_num_threads(nworkers);
+    std::cout << name << ": " << size << " " << nworkers << std::endl;
 
-  generate(size);
-  initialize(size);
+    omp_set_num_threads(nworkers);
 
-  auto begin = std::chrono::steady_clock::now();
-  run(size);
-  auto end = std::chrono::steady_clock::now();
-  auto elapsed =
-      std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+    generate(size);
+    initialize(size);
 
-  std::cout << "Elapsed time: " << elapsed.count() << "ms" << std::endl;
+    auto begin = steady_clock::now();
+    run(size);
+    auto end = steady_clock::now();
+    auto elapsed = duration_cast<milliseconds>(end - begin);
 
-  return 0;
+    std::cout << "A: " << std::endl;
+    print(a, size);
+    std::cout << "L: " << std::endl;
+    print(l, size);
+    std::cout << "U: " << std::endl;
+    print(u, size);
+
+    std::cout << "Elapsed time: " << elapsed.count() << "ms" << std::endl;
+    verify(size);
+
+    clean();
+
+    return 0;
 }
 
 void initialize(size_t size) {
-  lower = (double *)calloc(size * size, sizeof(double));
-  upper = (double *)calloc(size * size, sizeof(double));
-  permutation = (size_t *)calloc(size, sizeof(size_t));
+    l = new double[size * size];
+    u = new double[size * size];
+    p = new size_t[size];
 
-  for (size_t i = 0; i < size; i++) {
-    permutation[i] = i;
-    lower[i * size + i] = 1.0l;
-  }
+    for (size_t i = 0; i < size; i++) {
+        p[i] = i;
+        l[i * size + i] = 1.0l;
+    }
 }
 
 void generate(size_t size) {
-  std::random_device d;
-  std::default_random_engine e(d());
-  std::uniform_real_distribution dist(0.0l, 10.0l);
+    std::random_device d;
+    std::default_random_engine e(d());
+    std::uniform_real_distribution dist(0.0l, 10.0l);
 
-  target = (double *)malloc(sizeof(double) * size * size);
+    a = new double[size * size];
 
-  for (size_t i = 0; i < size; i++) {
-    for (size_t j = 0; j < size; j++) {
-      target[i * size + j] = dist(e);
+    for (size_t i = 0; i < size; i++) {
+        for (size_t j = 0; j < size; j++) {
+            a[i * size + j] = dist(e);
+        }
     }
-  }
 };
 
 void run(size_t size) {
-#pragma omp parallel shared(target, lower, upper, permutation)
-  for (size_t block = 0; block < size; block++) {
-    size_t pivot = block;
+#pragma omp parallel shared(a, l, u, p)
+    for (size_t k = 0; k < size; k++) {
+        size_t pivot = k;
 
-    #pragma omp for
-    for (size_t i = block; i < size; i++) {
-      pivot =
-          target[pivot * size + block] < target[i + size + block] ? i : pivot;
+#pragma omp for
+        for (size_t i = k; i < size; i++) {
+            if (abs(a[pivot * size + k]) < abs(a[i * size + k]))
+                pivot = i;
+        }
+
+        if (k != pivot) {
+            std::swap(p[k], p[pivot]);
+            std::swap_ranges(a + k * size, a + k * size + size,
+                             a + pivot * size);
+            std::swap_ranges(l + k * size, l + k * size + k, l + pivot * size);
+        }
+
+        u[k * size + k] = a[k * size + k];
+
+#pragma omp for
+        for (size_t i = k + 1; i < size; i++) {
+            l[i * size + k] = a[i * size + k] / u[k * size + k];
+            u[k * size + i] = a[k * size + i];
+        }
+
+#pragma omp for
+        for (size_t i = k + 1; i < size; i++) {
+            for (size_t j = k + 1; j < size; j++)
+                a[i * size + j] -= l[i * size + k] * u[k * size + j];
+        }
+    }
+}
+
+void verify(size_t size) {
+    double norm = 0.0;
+    double *r = new double[size * size];
+    double *lu = new double[size * size];
+
+#pragma omp parallel shared(p, a, l, u, r, norm)
+    {
+#pragma omp for
+        for (size_t i = 0; i < size; i++) {
+            for (size_t j = 0; j < size; j++) {
+                for (size_t k = 0; k < size; k++) {
+                    lu[i * size + j] += l[i * size + k] * u[k * size + j];
+                }
+            }
+        }
+
+        for (size_t i = 0; i < size; i++) {
+            for (size_t j = 0; j < size; j++) {
+                r[i * size + j] = a[i * size + j] - lu[i * size + j];
+            }
+        }
+
+#pragma omp for
+        for (size_t j = 0; j < size; j++) {
+            double sum = 0.0;
+
+            for (size_t i = 0; i < size; i++) {
+                sum += r[i * size + j] * r[i * size + j];
+            }
+
+#pragma omp critical
+            {
+                norm += sqrt(sum);
+            }
+        }
     }
 
-    size_t temp = permutation[block];
-    permutation[block] = permutation[pivot];
-    permutation[block] = temp;
+#pragma omp single
+    {
+        std::cout << "Residual: " << std::endl;
+        print(r, size);
+        std::cout << "PA: " << std::endl;
+        print(a, size);
+        std::cout << "LU: " << std::endl;
+        print(lu, size);
+    }
 
-    #pragma omp for
+    std::cout << "L2,1 norm of the residual matrix: " << norm << std::endl;
+    delete[] r;
+    delete[] lu;
+}
+
+void clean() {
+    delete[] a;
+    delete[] l;
+    delete[] u;
+    delete[] p;
+}
+
+void print(double *a, size_t size) {
+    std::cout << std::fixed << std::setprecision(2);
+
     for (size_t i = 0; i < size; i++) {
-      double temp = target[block * size + i];
-      target[block * size + i] = target[pivot * size + i];
-      target[pivot * size + i] = temp;
+        for (size_t j = 0; j < size; j++)
+            std::cout << std::setw(5) << a[i * size + j] << ' ';
+
+        std::cout << '\n';
     }
 
-    #pragma omp for
-    for (size_t i = 0; i < block; i++) {
-      double temp = lower[block * size + i];
-      lower[block * size + i] = lower[pivot * size + i];
-      lower[pivot * size + i] = temp;
-    }
-
-    upper[block * size + block] = target[block * size + block];
-
-    #pragma omp for
-    for (size_t i = block + 1; i < size; i++) {
-      lower[i * size + block] =
-          target[i * size + block] / upper[block * size + block];
-      upper[block * size + i] = target[block * size + i];
-    }
-
-    #pragma omp for 
-    for (size_t i = block + 1; i < size; i++) {
-      for (size_t j = block + 1; j < size; j++) {
-        target[i * size + j] -=
-            lower[i * size + block] * upper[block * size + j];
-      }
-    }
-  }
+    std::cout << '\n';
 }
