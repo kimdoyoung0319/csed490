6d1c499e0890e7ee9d506d803e78c5968a9938c7
Doyoung Kim <kimdoyoung0319@gmail.com>
2025-05-04 12:05:31 +0000

    Separated matrix type definition to another file.

---
+++ project2/src/matrix.h
@@ -0,0 +1,10 @@
+#ifndef MATRIX_H_
+#define MATRIX_H_
+
+#include <vector>
+
+/* Abbreviation for matrix and row type. */
+using Matrix = std::vector<std::vector<double>>;
+using Row = std::vector<double>;
+
+#endif
